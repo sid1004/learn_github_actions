d9368b70f8257cbb5de3b163b8a64691723d8ee8
Sidharth <82510112+sid1004@users.noreply.github.com>
2025-09-19 11:36:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask on EC2 deployed via GitHub Actions!"
+    return "Hello World from EC2 via GitHub Actions!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
